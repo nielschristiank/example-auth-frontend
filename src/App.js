820b357254b886c1a9e4dc02a1529f82f97fc0e8
Alyssa Hope <alyssamhope@gmail.com>
2018-09-11 10:53:51 -0700

    adds necessary packages (react router, jest, and enzyme), general cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Router>
+					<Switch>
+						<Route />
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
